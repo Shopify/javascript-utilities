c3d833910f51d1ae61007ae987bf545904327ab4
Utkarsh Saxena <utkarsh.saxena@shopify.com>
2017-06-02 11:51:27 -0400

    add debounce and memoize decorators

---
+++ src/debounce.ts
@@ -0,0 +1,33 @@
+import lodashDebounce, {DebounceSettings} from 'lodash/debounce';
+
+export function debounce(wait = 500, options?: DebounceSettings) {
+  return function(target: any, key: string, descriptor: PropertyDescriptor) {
+    const functionToDebounce = descriptor.value;
+
+    // prevents IE11 recursion error
+    let definingProperty = false;
+
+    // tslint:disable no-invalid-this
+    return {
+      configurable: true,
+      get() {
+        if (definingProperty || this === target.prototype || this.hasOwnProperty(key)) {
+          return functionToDebounce;
+        }
+
+        const boundFunction = lodashDebounce(functionToDebounce, wait, options);
+        definingProperty = true;
+
+        Object.defineProperty(this, key, {
+          value: boundFunction,
+          configurable: true,
+          writable: true,
+        });
+
+        definingProperty = false;
+        return boundFunction;
+      },
+    };
+    // tslint:enable
+  };
+}
---
+++ src/memoize.ts
@@ -0,0 +1,33 @@
+import loadashMemoize from 'lodash/memoize';
+
+export function memoize(resolver?: Function) {
+  return function(target: any, key: string, descriptor: PropertyDescriptor) {
+    const functionToMemoize = descriptor.value;
+
+    // prevents IE11 recursion error
+    let definingProperty = false;
+
+    // tslint:disable no-invalid-this
+    return {
+      configurable: true,
+      get() {
+        if (definingProperty || this === target.prototype || this.hasOwnProperty(key)) {
+          return functionToMemoize;
+        }
+
+        const boundFunction = loadashMemoize(functionToMemoize, resolver);
+        definingProperty = true;
+
+        Object.defineProperty(this, key, {
+          value: boundFunction,
+          configurable: true,
+          writable: true,
+        });
+
+        definingProperty = false;
+        return boundFunction;
+      },
+    };
+    // tslint:enable
+  };
+}
